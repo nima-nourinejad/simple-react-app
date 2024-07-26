fe1b92172d6783febb874af9db84e927ebac2e70
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-07-26 17:39:52 +0300

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+function App(props) {
+	const currDate = new Date();
+  
+	return (
+	  <div>
+		<h1>This is Nima!</h1>
+		<h2>The time now is {currDate.toLocaleTimeString()}.</h2>
+		<h2>The date now is {currDate.toDateString()}.</h2>
+	  </div>
+	);
+  }
+  
+  export default App;
